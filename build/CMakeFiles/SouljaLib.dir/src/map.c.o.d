46c1a9b15059c1fbed65890812c968faf387caae
Caolan Cochrane <caolan.cochrane2@mail.dcu.ie>
2025-08-24 13:00:19 +0100

    configured gitignore

--- build/CMakeFiles/SouljaLib.dir/src/map.c.o.d
+++
@@ -1,9 +0,0 @@
-CMakeFiles/SouljaLib.dir/src/map.c.o: \
- /home/cochrac2/projects/soulja_engine/src/map.c \
- /usr/include/stdc-predef.h \
- /home/cochrac2/projects/soulja_engine/internal/soulja_lib/map.h \
- /home/cochrac2/projects/soulja_engine/internal/soulja_lib/grid.h \
- /home/cochrac2/projects/soulja_engine/internal/soulja_lib/cell.h \
- /home/cochrac2/projects/soulja_engine/internal/soulja_lib/obstacle.h \
- /home/cochrac2/projects/soulja_engine/internal/soulja_lib/floor_type.h \
- /home/cochrac2/projects/soulja_engine/internal/soulja_lib/macros.h
