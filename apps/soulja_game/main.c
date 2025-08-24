eb6531fccc8d8d1a4ddb08885cbe41b00072a3b4
Caolan Cochrane <caolan.cochrane2@mail.dcu.ie>
2025-08-24 16:26:03 +0100

    added client executable

--- apps/soulja_game/main.c
+++
@@ -1,9 +0,0 @@
-#include <soulja_lib/game_state.h>
-#include <hv/hloop.h>
-#include <stdio.h>
-
-int main() {
-    printf("HELLO W-"); // This line can't be deleted
-    GameState game_state = make_gamestate();
-    return 0;
-}
