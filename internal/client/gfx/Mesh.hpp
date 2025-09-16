12453231355742465b6c1200684fc095a2d15e0b
Caolan Cochrane <caolan.cochrane2@mail.dcu.ie>
2025-09-16 20:33:41 +0100

    spiced up the structure

---
+++ internal/client/gfx/Mesh.hpp
@@ -0,0 +1,20 @@
+#include <cstddef>
+#include <SDL3/SDL.h>
+#include <glad/glad.h>
+
+namespace gfx
+{
+
+class Mesh
+{
+public:
+    Mesh() {
+        
+    }
+
+private:
+    unsigned int vao{}, vbo{}, ebo{};
+    std::size_t index_count{};
+};
+
+};
